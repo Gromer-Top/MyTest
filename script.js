60cb5375d3291485727fdc01c2d846b5f3e5023f
Gromer-Top <rus.solovey.09@mail.ru>
2024-11-22 13:15:18 +0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+window.addEventListener('load', function () {
+    // canvas setup
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;      
+})
